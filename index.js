f36551ac5a21f694d4fb7a2375d2410501dec00b
Anthony <kabuthuantony@gmail.com>
2024-03-20 23:29:23 +0300

    JS

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+// helpers.js
+let customerName = 'bob';
+const leastFavoriteCustomer = 'someone';
+
+function upperCaseCustomerName() {
+  customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(newBestCustomer) {
+  bestCustomer = newBestCustomer;
+}
+
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'another person';
+}
+
+module.exports = { customerName, upperCaseCustomerName, setBestCustomer, overwriteBestCustomer, changeLeastFavoriteCustomer };
